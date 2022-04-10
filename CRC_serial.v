a79c3fbc600ba723e368a45033b328c3ec7315ff
Avinash V <47024198+avnger5@users.noreply.github.com>
2022-04-10 22:20:29 +0530

    Add files via upload

---
+++ CRC_serial.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:04:21 04/09/2022 
+// Design Name: 
+// Module Name:    CRC_serial 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module CRC_serial(clk,reset,data_in,data_out);
+
+input clk,reset;
+input [9:0] data_in ;
+output reg [8:0] data_out;//,out;
+reg [9:0] data_reg;
+
+always @(posedge clk or posedge reset)
+begin
+if(reset)
+	begin
+	data_out <= 0;
+	$display("1  data_out = %b ",data_out);
+	data_reg <= data_in;
+	$display("1  data_in = %b ",data_reg);
+	end
+else
+	begin
+		
+		data_out[0] <= data_out[8]^data_reg[9];
+		data_out[1] <= data_out[0]^data_out[8]^data_reg[9];
+		data_out[2] <= data_out[1];
+		data_out[3] <= data_out[2];
+		data_out[4] <= data_out[3];
+		data_out[5] <= data_out[4];
+		data_out[6] <= data_out[5];
+		data_out[7] <= data_out[6];
+		data_out[8] <= data_out[7]^data_out[8]^data_reg[9];
+		
+		//data_reg <= data_reg<<1;
+		data_reg <= {data_reg[8:0],data_reg[9]};
+		
+		//out <= {data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]};
+		
+		$display($time," output = %b%b%b%b%b%b%b%b%b ",data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]);
+		//$display($time, " output = %b%b%b%b%b%b%b%b%b ",data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]);
+		
+	end
+	//out <= ~data_out;
+	//$display("2  out = %b ",out);
+end
+
+endmodule
+
+
+
---
+++ crc_test.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:08:08 04/09/2022
+// Design Name:   CRC_serial
+// Module Name:   C:/Users/hp/Desktop/Mapping DSP algo to Architecture/project/Project/CRC3/crc_test.v
+// Project Name:  CRC3
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: CRC_serial
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+///SERIAL CRC WORKING
+
+module crc_test;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg [9:0] data_in;
+
+	// Outputs
+	wire [8:0] data_out;
+	//wire [8:0] out;
+
+
+	// Instantiate the Unit Under Test (UUT)
+	CRC_serial uut (
+		.clk(clk), 
+		.reset(reset), 
+		.data_in(data_in), 
+		.data_out(data_out)
+	);
+//		.out(out)
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+		data_in = 10'b1100000011;
+		//data_in = 10'b1011001011;
+		
+		#10
+		reset = 0;
+	
+		
+		#10
+		reset = 0;
+		
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+		always #10 clk = ~clk;
+      
+endmodule
+
