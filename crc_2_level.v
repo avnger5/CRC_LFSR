5b26ad973d3a2c1e6c3265f6cf04dddff1181981
Avinash V <47024198+avnger5@users.noreply.github.com>
2022-04-10 22:42:42 +0530

    Add files via upload

---
+++ crc_2_level.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:59:32 04/10/2022 
+// Design Name: 
+// Module Name:    crc_2_level 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module crc_2_level(clk,reset,data_in,data_out);
+
+input clk,reset;
+input [9:0] data_in ;
+output reg [8:0] data_out;
+reg [9:0] data_reg;
+reg d1,d2;
+
+always @(posedge clk or posedge reset)
+begin
+if(reset)
+	begin
+	data_out <= 0;
+	$display("1  data_out = %b ",data_out);
+	data_reg <= data_in;
+	$display("1  data_in = %b ",data_reg);
+	d1 <= 0;
+	d2 <= 0;
+	end
+else
+	begin
+		
+		d1 <= data_reg[9];
+		data_out[0] <= d1^data_out[8];
+		data_out[1] <= d1^data_out[8]^data_out[0];
+		data_out[2] <= data_out[1];
+		data_out[3] <= data_out[2];
+		data_out[4] <= data_out[3];
+		data_out[5] <= data_out[4];
+		data_out[6] <= data_out[5];
+		data_out[7] <= data_out[6];
+		d2 <= data_out[7]^data_out[6]^data_reg[9]^d1^data_out[8];
+		data_out[8] <= d2;
+		
+		//data_reg <= data_reg<<1;
+		data_reg <= {data_reg[8:0],data_reg[9]};
+		
+		//out <= {data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]};
+		
+		$display($time," output = %b%b%b%b%b%b%b%b%b ",data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]);
+		
+	end
+	//out <= ~data_out;
+	//$display("2  out = %b ",out);
+end
+
+endmodule
+
+
+
---
+++ crc_test.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   12:05:29 04/10/2022
+// Design Name:   crc_2_level
+// Module Name:   C:/Users/hp/Desktop/Mapping DSP algo to Architecture/project/Project/twolevelcrc/crc_test.v
+// Project Name:  twolevelcrc
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: crc_2_level
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module crc_test;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg [9:0] data_in;
+
+	// Outputs
+	wire [8:0] data_out;
+	//wire [8:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	crc_2_level uut (
+		.clk(clk), 
+		.reset(reset), 
+		.data_in(data_in), 
+		.data_out(data_out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+		data_in = 10'b1100000011;
+		
+		#10
+		reset = 0;
+	
+		
+		#10
+		reset = 0;
+		
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+		always #10 clk = ~clk;
+      
+endmodule
+
