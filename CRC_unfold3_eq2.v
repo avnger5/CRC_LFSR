8770e8a8ab2dd25455a912d2776a5cea4bdfaaf8
Avinash V <47024198+avnger5@users.noreply.github.com>
2022-04-13 15:42:53 +0530

    Add files via upload
    Serial CRC for equation 1+y+y3+y5
    
    3 level Unfolded

---
+++ CRC_unfold3_eq2.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:20:14 04/13/2022 
+// Design Name: 
+// Module Name:    CRC_unfold3_eq2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module CRC_unfold3_eq2(clk,reset,data_in,data_out);
+
+input clk,reset;
+input [5:0] data_in ;
+output reg [4:0] data_out;
+reg [5:0] data_reg;
+
+
+always @(posedge clk or posedge reset)
+begin
+if(reset)
+	begin
+	data_out <= 0;
+	$display("1  data_out = %b ",data_out);
+	data_reg <= data_in;
+	$display("1  data_in = %b ",data_reg);
+	end
+else
+	begin
+		
+		data_out[0] <= data_reg[3]^data_out[2]^data_reg[5]^data_out[4];
+		data_out[1] <= data_reg[3]^data_out[2]^data_reg[5]^data_out[4]^data_reg[4]^data_out[3];
+		data_out[2] <= data_reg[5]^data_reg[4]^data_out[3]^data_out[4];
+		data_out[3] <= data_reg[3]^data_out[2]^data_reg[5]^data_out[4]^data_out[0]^data_out[4]^data_reg[5];
+		data_out[4] <= data_out[1]^data_out[3]^data_reg[4];
+		
+		
+		//data_reg <= data_reg<<2;
+		data_reg <= {data_reg[2:0],data_reg[5],data_reg[4],data_reg[3]};
+	
+		//out <= {data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]};
+		$display($time," output = %b ",data_out);
+		//$display($time," output = %b%b%b%b%b%b%b%b%b ",data_out[0],data_out[1],data_out[2],data_out[3],data_out[4],data_out[5],data_out[6],data_out[7],data_out[8]);
+		//$display($time," data = %b ",data_reg);
+	end
+	//out <= ~data_out;
+	//$display("2  out = %b ",out);
+end
+
+endmodule
---
+++ CRC_unfold3_eq2_test.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:36:25 04/13/2022
+// Design Name:   CRC_unfold3_eq2
+// Module Name:   C:/Users/hp/Desktop/Mapping DSP algo to Architecture/project/Project/CRC_unfold3_eq2/CRC_unfold3_eq2_test.v
+// Project Name:  CRC_unfold3_eq2
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: CRC_unfold3_eq2
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module CRC_unfold3_eq2_test;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg [5:0] data_in;
+
+	// Outputs
+	wire [4:0] data_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	CRC_unfold3_eq2 uut (
+		.clk(clk), 
+		.reset(reset), 
+		.data_in(data_in), 
+		.data_out(data_out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+		//data_in = 10'b101011;
+		data_in = 10'b111111;
+		
+		#10
+		reset = 0;
+	
+		
+		#10
+		reset = 0;
+		
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+		always #10 clk = ~clk;
+      
+endmodule
+
+
+
+
+
